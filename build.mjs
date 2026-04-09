57b1436972bad0591e154c998dbb783d3ffe2e16
mnem <mnem424242@gmail.com>
2026-04-09 23:49:44 +0100

    feat: general improvements

--- build.mjs
+++ build.mjs
@@ -1,12 +1,16 @@
 import * as esbuild from 'esbuild'
 import * as fs from 'fs'
+import { createServer } from 'http'
 
 // Change this to whatever the mod should be called, without the extension.
 const MOD_NAME = "test"
 
 // Change this to something else if you want to make it go somewhere that it
 // normally wouldn't
-const OUT_FILE = `sandboxels/mods/${MOD_NAME}.js`
+const OUT_FILE = `sandboxels-mods/mods/${MOD_NAME}.js`
+
+const host_server = process.env.HOST_SERVER === "true"
+const server_port = parseInt(process.env.SERVER_PORT) || 3000
 
 await esbuild.build({
     entryPoints: ['src/main.ts'],
@@ -26,10 +30,18 @@ await esbuild.build({
     banner: {
         js: `// ${MOD_NAME}.js`
     },
-}).then(async (result) =>{
-    let stats = fs.statSync(`sandboxels/mods/${MOD_NAME}.js`)
+}).then(() => {
+    let stats = fs.statSync(`sandboxels-mods/mods/${MOD_NAME}.js`)
     console.log("Build finished")
     console.log("Build output size: ", stats.size, "(B)")
 
-    fs.copyFile(`sandboxels/mods/${MOD_NAME}.js`, `sandboxels-mods/mods/${MOD_NAME}.js`, () => console.log("File copied"))
+    if (host_server) {
+        const code = fs.readFileSync(`sandboxels-mods/mods/${MOD_NAME}.js`)
+
+        const server = createServer((req, res) => {
+            res.writeHead(200, { 'Content-Type': 'text/javascript' })
+            res.end(code)
+        })
+        server.listen(server_port)
+    }
 })
